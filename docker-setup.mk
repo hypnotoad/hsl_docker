0d2f860bc91772e824807e9cb04b1d02ba0c746d
Ralf Dragon <hypnotoad@lindra.de>
2025-06-08 15:51:45 +0200

    Update

--- docker-setup.mk
+++ docker-setup.mk
@@ -1,3 +1,20 @@
 user = $(shell id --user):$(shell id --group)
-hslfile = $(number)_$(name).hsl
-python2 = docker run --user ${user} --volume $(shell pwd):/hsl/projects/$(name) --workdir /hsl lindra/gira /usr/bin/python2
+hslfile = release/$(number)_$(name).hsl
+
+python2 = docker run --user ${user} --volume "$(shell pwd):/hsl/projects/$(name)" --workdir /hsl lindra/gira /usr/bin/python2
+
+
+.PHONY: clean default
+
+default: $(hslfile)
+
+$(hslfile): config.xml src/*.py
+	$(python2) generator.pyc "$(name)"
+
+$(hslfile).zip: $(hslfile)
+	zip "$@" "$^"
+
+clean:
+	rm -f "$(hslfile)"
+
+
